c39236d20a1c9a9e299674636bf5441d1e509174
nickrnw <nickrd@pm.me>
2019-11-29 16:40:06 -0800

    Added Header & Links to/from home/shop

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shoppage.component';
 
+import Header from '../src/components/header/header.component';
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+/* Logo */
+import { ReactComponent as Logo } from '../../assets/shopping-cart.svg';
+
+/* Styles */
+import '../header/header.styles.css';
+
+const Header = () => (
+
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'></Logo>
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                Shop
+            </Link>
+            <Link className='option' to='/contact'>
+                Contact
+            </Link>
+        </div>
+    </div>
+);
+
+export default Header;
